ff45165f4371073d943a9cf133466a1aff3acc93
Derp88 <62079853+Derp88@users.noreply.github.com>
2021-04-06 20:17:06 -0400

    changed autobounce
    suffering

--- src/main/java/frc/robot/commands/AutoBounce.java
+++ src/main/java/frc/robot/commands/AutoBounce.java
@@ -23,19 +23,19 @@ public class AutoBounce extends SequentialCommandGroup {
    // super();
    DriveSubsystem = s_DriveSubsystem;
 
-   double c_visionFirstAllDistance = 6.7;
+   double c_visionFirstAllDistance = 3.210;
    double c_driveToFirstBackupArea = 5.84;
 
-   double c_driveToFirstArea = 0.81;
+   double c_driveToFirstArea = 2.01;
 
-   double c_driveToSecondTarget = 15.71;
-   double c_visionSecondCloseDistance = 16.44;
-   double c_driveToSecondBackupTarget = 5.67;
-   double c_visionSecondBackupDistance = 4.88;
+   double c_driveToSecondTarget = 3;
+   double c_visionSecondCloseDistance = 6.2;
+   double c_driveToSecondBackupTarget = 2.683;
+   double c_visionSecondBackupDistance = 1.519;
 
-   double c_driveToThirdArea = 2.18;
-   double c_driveToThirdTarget = 1.97;
-   double c_visionThirdCloseDistance = 2.91;
+   double c_driveToThirdArea = 3.680;
+   double c_driveToThirdTarget = 22.323-.4;
+   double c_visionThirdCloseDistance = 22.323;
 
    double c_driveToThirdBackupTarget = 0.93;
    double c_visionThirdBackupDistance = 1.53;
@@ -45,51 +45,61 @@ public class AutoBounce extends SequentialCommandGroup {
 
    addCommands(
      //Inital Straight
-     new AutoForwardPID(s_DriveSubsystem, .7, .65, 12),
+     /*
+     new AutoForwardPID(s_DriveSubsystem, .7, .65, 16),
      new AutoForward(s_DriveSubsystem, -0.65, -0.65, .2),
 
      //First Left to get us to first target
-     new AutoRight90Gyro(s_DriveSubsystem, 90, -.35, .7),
+     new AutoRight90Gyro(s_DriveSubsystem, 90, -.45, .7),
 
      //Touch First Target
      new VisionAll(s_DriveSubsystem, c_visionFirstAllDistance),
 
      //Backup from Target after contact
      //new DriveToVisionTargetArea(s_DriveSubsystem, -.65, -.65, c_driveToFirstBackupArea),
-     new AutoForwardPID(s_DriveSubsystem, -.7, -.65, 12),
+     new AutoForwardPID(s_DriveSubsystem, -.7, -.65, 8),
      new AutoForward(s_DriveSubsystem, 0.65, 0.65, .2),
 
      //First S-Turn (Reversing)
      new AutoRight90Gyro(s_DriveSubsystem, 45, -.7, .35),
+     new AutoForward(s_DriveSubsystem, 0.65, 0.0, .2),
+     new AutoForwardPID(s_DriveSubsystem, -.5, -.45, 15),
      new AutoForward(s_DriveSubsystem, 0.65, 0.65, .2),
-     new AutoForwardPID(s_DriveSubsystem, -.5, -.45, 12)
-    /* new AutoForward(s_DriveSubsystem, 0.65, 0.65, .2),
      new AutoRight90Gyro(s_DriveSubsystem, 45, .35, -.7),
 
-
      //Backup After S-Turn and get to position for second target
-     new AutoForwardPID(s_DriveSubsystem, -.7, -.65, 30),
-     new AutoForward(s_DriveSubsystem, 0.65, 0.65, .2)
-     /*
-     new AutoRight90Gyro(s_DriveSubsystem, 90, .55, -.55),
-     new VisionAlign(s_DriveSubsystem),
-     new DriveToVisionTargetArea(s_DriveSubsystem, .65, .65, c_driveToFirstArea),
-     new AutoForward(s_DriveSubsystem, -0.65, -0.65, .2),
+     new AutoForwardPID(s_DriveSubsystem, -.7, -.65, 12),
+     new AutoForward(s_DriveSubsystem, 0.65, 0.65, .3),
+     new AutoRight90Gyro(s_DriveSubsystem, 70, .7, -.7),
+     //new VisionAlign(s_DriveSubsystem),
+     //new AutoForwardPID(s_DriveSubsystem, .7, .65, 30),
+     //new AutoForward(s_DriveSubsystem, 0.65, 0.65, .2),
+     
+     */
+     
+     //new VisionAlign(s_DriveSubsystem),
+     //new DriveToVisionTargetArea(s_DriveSubsystem, .65, .65, c_driveToFirstArea),
+     new VisionAll(s_DriveSubsystem, c_driveToFirstArea),
+     //new AutoForward(s_DriveSubsystem, -0.65, -0.65, .3),
 
      //Turn and align to second target
-     new AutoRight90Gyro(s_DriveSubsystem, 90, -.35, .7),
+     new AutoRight90Gyro(s_DriveSubsystem, 90, -.55, .7),
      new VisionAlign(s_DriveSubsystem),
      new DriveToVisionTargetArea(s_DriveSubsystem, .65, .65, c_driveToSecondTarget),
      new AutoForward(s_DriveSubsystem, -0.65, -0.65, .2),
      new VisionAll(s_DriveSubsystem, c_visionSecondCloseDistance),
 
      //Backup
-     new DriveToVisionTargetArea(s_DriveSubsystem, -.65, -.65, c_driveToSecondBackupTarget),
-     new AutoForward(s_DriveSubsystem, 0.65, 0.65, .2),
-     new VisionAll(s_DriveSubsystem, c_visionSecondBackupDistance),
+     //new DriveToVisionTargetArea(s_DriveSubsystem, -.65, -.65, c_driveToSecondBackupTarget),
+     //new AutoForward(s_DriveSubsystem, 0.65, 0.65, .2),
+     //new VisionAll(s_DriveSubsystem, c_visionSecondBackupDistance)
+     new DriveToVisionTargetArea(s_DriveSubsystem, -0.65, -0.65, c_visionSecondBackupDistance),
+  //   new AutoForwardPID(s_DriveSubsystem, -0.51, -0.5, 80)
+     new AutoForward(s_DriveSubsystem, 0.80, 0.80, .3)
 
+     /*
      //Go forward to final target area
-     new AutoRight90Gyro(s_DriveSubsystem, 90, .7, -.35),
+     new AutoRight90Gyro(s_DriveSubsystem, 90, .7, -.45),
      new VisionAlign(s_DriveSubsystem),
      new DriveToVisionTargetArea(s_DriveSubsystem, .65, .65, c_driveToThirdArea),
      new AutoForward(s_DriveSubsystem, -0.65, -0.65, .2),
@@ -102,14 +112,14 @@ public class AutoBounce extends SequentialCommandGroup {
      new VisionAll(s_DriveSubsystem, c_visionThirdCloseDistance),
 
      //Backup after contact with third target
-     new DriveToVisionTargetArea(s_DriveSubsystem, -.65, -.65, c_driveToThirdBackupTarget),
-     new AutoForward(s_DriveSubsystem, 0.65, 0.65, .2),
-     new VisionAll(s_DriveSubsystem, c_visionThirdBackupDistance),
+     //new DriveToVisionTargetArea(s_DriveSubsystem, -.65, -.65, c_driveToThirdBackupTarget),
+     //new AutoForward(s_DriveSubsystem, 0.65, 0.65, .2),
+     //new VisionAll(s_DriveSubsystem, c_visionThirdBackupDistance),
 
      //Turn to end zone
-     new AutoRight90Gyro(s_DriveSubsystem, 90, .7, -.35),
-     new AutoForwardPID(s_DriveSubsystem, .7, .65, 18),
-     new DriveUntilBounceSensor(s_DriveSubsystem, .3, .3)
+     new AutoRight90Gyro(s_DriveSubsystem, 90, .7, -.35)
+     //new AutoForwardPID(s_DriveSubsystem, .7, .65, 18),
+     //new DriveUntilBounceSensor(s_DriveSubsystem, .3, .3)
      */
    );
   }
